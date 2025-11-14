81b65dea014aa723d8792a07e2e06a54235b93cc
Gastremo <Remi.belmans@hotmail.com>
2025-11-14 16:39:46 +0100

    Update .c

--- .c
+++ .c
@@ -1,1 +1,227 @@
+//(c) Gastremo 2025
+//(c)Spacex
+//SpaceX-G software
+//(p)Greenzone_G 2025
+//(p)Greenzone 2025
+//<>
+//(r)SpaceX-G
+Printf("Welkom bij GASTREMO SpaceX-G")
+printf("1.0.50")
+#define AUTEUR "Remi Gastremo Belmans"
+#define TAAL ".c,.h"
+#define BEDRIJF "Gastremo"
+#define GROEP "the legends softwares"
+#define ORGANISATIE "Gastremo-project"
+#define SOFTWARE "SpaceX-G"
+#define ELEMENTBRON "stelarium"
+#define BETALINGSPLATFORMEN "visa,bankcontact,payconic,payal"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <conecties.h>
+#include <Layout.h>
+#include <Account.h>
+#include <index.h>
+#include <register.h>
+#include <catalogus.h>
+#include <register.h>
+#include <elementen.h>
+#include <instal.h>
+#include <Run.scss>
+#include <Instal_windows.syx>
+#include <Windows.exe>
+#include <Instal_mac.syx>
+#include <mac.exe>
+#include <Instal_linux.syx>
+#include <linux.exe>
+}
+
+int main();
+
+
+int layout;
+int opstart;
+int help;
+int system;
+int sneltoetsen;
+int instal;
+int launge;
+int exentions;
+int Pay;
+int SpaceX-G-Studio;
+int Contiburing;
+int Codeofconduct;
+
+double elements
+double Indexorregister
+
+char Conect_internet;
+char unconnect_internet
+char Conect_bleuthoot
+char Unconect_bleuthoot;
+char Conect_database;
+char Unconect_database;
+char Connect_SAT
+char Unconnect_SAT
+char Text;
+char Telescope_Data;
+char Auto_save;
+char Auto_run;
+char Auto_pay;
+char Auto_lock;
+char Account
+char Account_make;
+char Account_name;
+char Account_e-mail;
+char Account_pasword;
+char Account_Male|Female;
+char Account_connect_apps;
+char Account_connect_otheraccount;
+char Account_byte_run;
+char Account_delete;
+char Account_upgrade;
+char Account_downgrade;
+char Account_type;
+char Server;
+char Layout_background_color;
+char Layout_textcolor;
+char Layout_persionalisize;
+char Layout_standard;
+char Elementinformation_name;
+char Elementinformation_heigt;
+char Elementinformation_AE;
+char Elementinformation_LI;
+char Elementinformation_size;
+char Elementinformation_type;
+char Elementinformation_colorindex;
+char Elementinformation_Mangitude;
+char Exit;
+char AutoLogIn;
+char My profile;
+char Logout;
+char Button_time_back;
+char Button_time_normal;
+char Button_time_forward;
+char Button_time_stop;
+char Button_time_play;
+char Button_time_pause;
+char Button_gotostelarium;
+char Button_exit;
+char Button_constellationlines
+char Button_zodiacart
+char Button_zodiacsignlabels
+char Show_sidebar;
+char Show bottombar;
+char Menu_view;
+char Menu_account;
+char Menu_extra;
+char Menu_help;
+char Menu_index;
+char Menu_register;
+char Menu_catalogus;
+char window_search_element;
+char window_search_SAT;
+char Window_search_telescpe;
+char Window_astronomicalcalculations;
+char Window_setings_location;
+char Window_setings_time;
+char timer_forward;
+char timer_back;
+char timer_stop;
+char timer_start;
+char timer_pause;
+char pay_run
+char pay_verficate_cardnumber
+char pay_connect_payaccount
+char pay_connect_administrator
+char verficate_robot
+char Modus_3D
+char Mudus_card
+char Modus_skymap
+char catalogus_elements_order
+char catalogus_elements_information
+char catalogus_manager
+char index_A
+char index_B
+char index_C
+char index_D
+char index_E
+char index_F
+char index_G
+char index_H
+char index_I
+char index_J
+char index_K
+char index_L
+char index_M
+char index_N
+char index_O
+char index_P
+char index_Q
+char index_R
+char index_S
+char index_T
+char index_U
+char index_V
+char index_W
+char index_X
+char index_Y
+char index_Z
+char index_1
+char index_2
+char index_3
+char index_4
+char index_5
+char index_6
+char index_7
+char index_8
+char index_9
+char register_A
+char register_B
+char register_C
+char register_D
+char register_E
+char register_F
+char register_G
+char register_H
+char register_I
+char register_J
+char register_K
+char register_L
+char register_M
+char register_N
+char register_O
+char register_P
+char register_Q
+char register_R
+char register_S
+char register_T
+char register_U
+char register_V
+char register_W
+char register_X
+char register_Y
+char register_Z
+char register_1
+char register_2
+char register_3
+char register_4
+char register_5
+char register_6
+char register_7
+char register_8
+char register_9
+char catalogus_1
+char catalogus_2
+char catalogus_3
+char catalogus_4
+char catalogus_5
+char catalogus_6
+char catalogus_7
+char catalogus_8
+char catalogus_9
+char c_export
+char c_return
+}
+return(0)
+}
